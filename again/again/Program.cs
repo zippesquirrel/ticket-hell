bfb03819deefcc1e51ee6625086374a23002de23
zippesquirrel <maxkinsberg@gmail.com>
2026-02-01 11:42:55 -0700

    add looper

--- again/again/Program.cs
+++ again/again/Program.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System.Xml;
+﻿using System.Xml;
 
 namespace again
 {
@@ -12,11 +10,40 @@ namespace again
     {
         static void Main(string[] args)
         {
-            string name = Prompt("What is your name? ", false);
-            int nameLength = name.Replace(" ", "").Length;
-            int nameWords = 1 + name.Trim().Count(f => f == ' ');
+            //string name = Prompt("What is your name? ", false);
+            //int nameLength = name.Replace(" ", "").Length;
+            //int nameWords = 1 + name.Trim().Count(f => f == ' ');
 
-            Console.WriteLine($"Your name is {TitleCase(name.Trim())}, your name has {nameLength} letters, and is {nameWords} words.");
+            //Console.WriteLine($"Your name is {TitleCase(name.Trim())}, your name has {nameLength} letters, and is {nameWords} words.");
+            for (int i = 0; i < 100; i++) 
+            {
+                Console.WriteLine(Looper("", i));
+            }
+        }
+        static string Looper(string str, int frame)
+        {
+            string result = "";
+            int frameMod = frame % 4;
+            switch (frameMod)
+            {
+                case 0:
+                    result = "12345";
+                    break;
+                case 1:
+                    result = "23451";
+                    break;
+                case 2:
+                    result = "34512";
+                    break;
+                case 3:
+                    result = "45123";
+                    break;
+                case 4:
+                    result = "51234";
+                    break;
+            }
+            Thread.Sleep(50);
+            return result;
         }
         static string TitleCase(string str)
         {
